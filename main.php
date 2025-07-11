1cffd2ebb697f8df760076fd7ec3f6b5039b3952
FASTO96 <60965678+FASTO96@users.noreply.github.com>
2025-07-11 11:40:28 -0400

    Add PHP server files: main.php, Dockerfile, version.txt, lg.txt as specified

--- main.php
+++ main.php
@@ -1,5 +1,9 @@
 <?php
-// main.php: Simple PHP server script
-
-echo "welcome home 7<br>";
-echo "sum: ".(21+5);
+// Print welcome message 7 times
+for ($i = 0; $i < 7; $i++) {
+    echo "welcome homex 7<br>\n";
+}
+// Print sum of 20 + 5
+$sum = 20 + 5;
+echo "Sum: $sum\n";
+?>
